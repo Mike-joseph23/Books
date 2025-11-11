f295aca686e63d75d5ae03cdea54939c5218d396
Mike-joseph23 <202302609@umh.edu.hn>
2025-11-10 21:13:43 -0600

    actualizacion

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using books.Models;
+
+namespace books.Controllers
+{
+    public class BooksController : Controller
+    {
+        Book mybook = new Book("El general y su laberinto", "Gabriel Garcia Marquez", 1995);
+
+        // GET: BooksController
+        public ActionResult Index()
+        {
+            return View(mybook);
+        }
+
+        // GET: BooksController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BooksController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BooksController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace books.Models
+{
+    public class Book
+    {
+        public string titulo;
+        public string autor;
+        public int anio;
+
+
+        public Book(string p_titulo, string p_autor, int p_anio)
+        {
+
+            this.anio = p_anio;
+            this.autor = p_autor;
+            this.titulo = p_titulo;
+
+        }
+    }
+}
